471acf62d12c57b9611efe6805323a511a47e6d2
Stetson Done <scdone@gmail.com>
2021-12-21 12:26:13 -0700

    first 3 functions completed

--- server/controller.js
+++ server/controller.js
@@ -1,13 +1,32 @@
 const houses = require(`./db.json`);
+let newHouseId = 4;
 
 module.exports = {
-    getHouses: (req,res) => {
-        req.status(200).send(houses)
+    getAllHouses: (req,res) => {
+        res.status(200).send(houses)
     },
     deleteHouse: (req, res) => {
+        const targetId = +req.params.id;
+
+        const foundIndex = houses.findIndex(houseObj => {
+            return houseObj.id === targetId
+        });
+
+        houses.splice(foundIndex, 1)
+
+        res.status(200).send(houses);
 
     },
     createHouse: (req, res) => {
+        console.log(req.body)
+
+        req.body.id = newHouseId
+
+        houses.push(req.body)
+
+        newHouseId += 1;
+
+        res.status(200).send(houses);
 
     },
     updateHouse: (req, res) => {
--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const express = require(`express`);
 const cors = require(`cors`);
 const ctrl = require(`./controller`)
 
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get(`/api/houses`, ctrl.getHouses)
+app.get(`/api/houses`, ctrl.getAllHouses)
 app.post(`/api/houses`, ctrl.createHouse)
 app.put(`/api/houses/:id`, ctrl.updateHouse)
 app.delete(`/api/houses/:id`, ctrl.deleteHouse)
